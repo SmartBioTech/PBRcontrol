48226fc069a8601facecac2e1e3b01f0ae97bdca
Daniel Magoc <485173@mail.muni.cz>
2019-09-24 13:58:27 +0200

    documentation updated

--- DBmanager/measurement.py
+++ DBmanager/measurement.py
@@ -7,6 +7,16 @@ import datetime
 class PeriodicalMeasurement(Thread):
 
     def execute_cmd(self, time_issued, cmd_id, args, source):
+        """
+        Execute command which affects the measurement and or all devices on the node it runs on
+
+        :param time_issued: str
+        :param cmd_id: int
+        :param args: list
+        :param source: str
+
+        :return: None
+        """
         is_ok = True
         try:
             args = eval(args)
@@ -14,21 +24,33 @@ class PeriodicalMeasurement(Thread):
         except Exception as exc:
             response = exc
             is_ok = False
+
+        # save the response to log
         self.logger.update_log(time_issued, self.node_id, 'None', cmd_id, args, (is_ok,response), source)
 
     def change_time_period(self, t):
+        """
+        Command used to change thee interval of measurements
+
+        :param t: float
+        :return: True if ok -> if not ok, error must occur
+        """
 
         self.experiment_details['sleep_time'] = t
         return True
 
     def __init__(self, node_id, devices, experiment_details):
+        """
+        :param node_id: int
+        :param devices: list of devices on the node
+        :param experiment_details: dict
+        """
         super(PeriodicalMeasurement, self).__init__(name=(str(node_id)+'-measurement'))
         self.experiment_details = experiment_details
         self.end_measurement = Event()
         self.devices = devices
         self.node_id = node_id
         self.logger = localdb.Database()
-        self.logger.connect()
 
         self.codes = {'PBR': 19,
                       'GAS': 28}
@@ -36,15 +58,24 @@ class PeriodicalMeasurement(Thread):
         self.commands = {35: self.change_time_period}
 
     def run(self):
+        """
+        Every t seconds perform measurement
 
+        :return: None
+        """
         next_time = time.time() + self.experiment_details['sleep_time']
-        while not self.end_measurement.is_set():
+        while not self.end_measurement.is_set():    # while the end signal wasn't sent
             time.sleep(max(0, next_time - time.time()))
             delay = self.experiment_details['sleep_time']
             self.measure()
             next_time += (time.time() - next_time) // delay * delay + delay
 
     def measure(self):
+        """
+        Forward the measure all command to all devices on the node
+
+        :return:  None
+        """
         for device_key in self.devices:
             device = self.devices[device_key]
             if device == 'GMS':
@@ -56,8 +87,12 @@ class PeriodicalMeasurement(Thread):
 
             device.accept_command(cmd)
 
-
     def end(self):
+        """
+        Set a flag to end measurement
+
+        :return: None
+        """
         self.end_measurement.set()
 
 
