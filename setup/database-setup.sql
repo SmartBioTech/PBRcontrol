91ba0675f4b56e198bcde63cd91bfdee95660c73
Daniel Magoc <xmagoc@fi.muni.cz>
2019-10-29 10:19:19 +0100

    sql script edited

--- setup/database-setup.sql
+++ setup/database-setup.sql
@@ -1,3 +1,3 @@
-create user if not exists 'PBRcontrol'@'localhost';
+create user if not exists 'PBRcontrol'@'localhost' identified by '&Bioairneo1';
 create database if not exists localdb;
 grant all privileges on localdb.* to 'PBRcontrol'@'localhost' identified by '&Bioairneo1';
