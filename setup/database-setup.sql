977e26846eaca3c6a453fddb824d8e39fc028cf7
Daniel Magoc <xmagoc@fi.muni.cz>
2019-11-24 14:21:17 +0100

    password to db corrected

--- setup/database-setup.sql
+++ setup/database-setup.sql
@@ -1,3 +1,3 @@
-create user if not exists 'PBRcontrol'@'localhost' identified by '&Bioairneo1';
+create user if not exists 'PBRcontrol'@'localhost' identified by '&Bioarineo1';
 create database if not exists localdb;
-grant all privileges on localdb.* to 'PBRcontrol'@'localhost' identified by '&Bioairneo1';
+grant all privileges on localdb.* to 'PBRcontrol'@'localhost' identified by '&Bioarineo1';
