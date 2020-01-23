38f88435da25d2adcf15840604e9a8ec949475bf
Daniel Magoc <xmagoc@fi.muni.cz>
2020-01-23 23:45:22 +0100

    all PBR, GAS and GMS methods added to base abstract device for easier implementation on higher levels

--- HWdevices/abstract/device.py
+++ HWdevices/abstract/device.py
@@ -11,3 +11,354 @@ class Device:
 
     def disconnect(self):
         print('Test device ' + self.ID + ' is disconnecting')
+
+    def get_temp_settings(self):
+        """
+        Get information about currently set temperature, maximal and
+        minimal allowed temperature.
+
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_temp(self):
+        """
+        Get current temperature in Celsius degree.
+
+        :return: The current temperature.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_temp(self, temp):
+        """
+        Set desired temperature in Celsius degree.
+
+        :param temp: The temperature.
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_ph(self):
+        """
+        Get current pH (dimensionless.)
+
+        :param repeats: the number of measurement repeats
+        :param wait: waiting time between individual repeats
+        :return: The current pH.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def measure_od(self, channel=0):
+        """
+        Measure current Optical Density (OD, dimensionless).
+
+        :param channel: which channel should be measured
+        :param repeats: the number of measurement repeats
+        :return: Measured OD
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_pump_params(self, pump):
+        """
+        Get parameters for given pump.
+
+        :param pump: Given pump
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_pump_params(self, pump, direction, flow):
+        """
+        Set up the rotation direction and flow for given pump.
+
+        :param pump: Given pump
+        :param direction: Rotation direction (1 right, -1 left)
+        :param flow: Desired flow rate
+        :return:  True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_pump_state(self, pump, on):
+        """
+        Turns on/off given pump.
+
+        :param pump: ID of a pump
+        :param on: True to turn on, False to turn off
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_light_intensity(self, channel):
+        """
+        Checks for current (max?) light intensity.
+
+        Items: "intensity": current light intensity (float) in μE,
+               "max": maximal intensity (float) in μE,
+               "on": True if light is turned on (bool)
+
+        :param channel: Given channel ID
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_light_intensity(self, channel, intensity):
+        """
+        Control LED panel on photobioreactor.
+
+        :param channel: Given channel (0 for red light, 1 for blue light)
+        :param intensity: Desired intensity
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def turn_on_light(self, channel, on):
+        """
+        Turn on/off LED panel on photobioreactor.
+
+        :param channel: Given channel
+        :param on: True turns on, False turns off
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_pwm_settings(self):
+        """
+        Checks for current stirring settings.
+
+        Items: "pulse": current stirring in %,
+               "min": minimal stirring in %,
+               "max": maximal stirring in %,
+               "on": True if stirring is turned on (bool)
+
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_pwm(self, value, on):
+        """
+        Set stirring settings.
+        Channel: 0 red and 1 blue according to PBR configuration.
+
+        :param value: desired stirring pulse
+        :param on: True turns on, False turns off
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_o2(self, raw=True, repeats=5, wait=0):
+        """
+        Checks for concentration of dissociated O2.
+
+        Items: "pulse": current stirring in %,
+               "min": minimal stirring in %,
+               "max": maximal stirring in %,
+               "on": True if stirring is turned on (bool)
+
+        :param raw: True for raw data, False for data calculated according to temperature calibration
+        :param repeats: the number of measurement repeats
+        :param wait: waiting time between individual repeats
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_thermoregulator_settings(self):
+        """
+        Get current settings of thermoregulator.
+
+        Items: "temp": current temperature in Celsius degrees,
+               "min": minimal allowed temperature,
+               "max": maximal allowed temperature,
+               "on": state of thermoregulator (1 -> on, 0 -> freeze, -1 -> off)
+
+        :return: The current settings structured in a dictionary.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_thermoregulator_state(self, on):
+        """
+        Set state of thermoregulator.
+
+        :param on: 1 -> on, 0 -> freeze, -1 -> off
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def measure_ft(self, channel):
+        """
+        ???
+
+        :param channel: ???
+        :return: ???
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_co2(self, raw, repeats):
+        """
+        TBA
+
+        :param raw: True for raw data, False for data ???
+        :param repeats: the number of measurement repeats
+        :return:
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def measure_all(self, ft_channel=5, pump_id=5):
+        """
+        Measures all basic measurable values.
+
+        :param ft_channel: channel for ft_measure
+        :param pump_id: id of particular pump
+        :return: dictionary of all measured values
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def measure_AUX(self, channel):
+        """
+        Values of AUX auxiliary input voltage.
+
+        :param channel: ???
+        :return: ???
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def flash_LED(self):
+        """
+        Triggers a flashing sequence and is used to physically identify the PBR.
+
+        :return: True if was successful, False otherwise
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_hardware_address(self):
+        """
+        Get the MAC address of the PBR.
+
+        :return: the MAC address
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_cluster_name(self):
+        """
+        The name of the bioreactor array / cluster.
+
+        :return: the cluster name
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+    
+    def get_valve_flow(self, valve):
+        """
+        Get value (L/min) of current flow in the given valve.
+
+        :param valve: ID of the valve (0 for CO2, 1 for Air)
+        :return: The current settings of the valve flow and actual value, both in (L/min).
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_valve_flow(self, valve, value):
+        """
+        Set value (L/min) of current flow in the given valve.
+
+        :param valve: ID of the valve (0 for CO2, 1 for Air)
+        :param value: desired value for valve flow in (L/min).
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_valve_info(self, valve):
+        """
+        Gives information about the valve
+
+        :param valve: ID of the valve (0 for CO2, 1 for Air)
+        :return: A dictionary with gas type and maximal allowed flow.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+
+    def get_co2_air(self):
+        """
+        Measures CO2 in air.
+
+        :return: measured CO2 in air
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_small_valves(self):
+        """
+        Obtain settings of individual vents of GAS device.
+
+        Represented as one byte, where first 6 bits represent
+        vents indexed as in a picture scheme available here:
+        https://i.imgur.com/jSeFFaO.jpg
+
+        :return: byte representation of vents settings.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_small_valves(self, mode):
+        """
+        Changes settings of individual vents of GAS device.
+
+        Can be set by one byte (converted to int), where first 6
+        bits represent vents indexed as in a picture scheme
+        available here: https://i.imgur.com/jSeFFaO.jpg
+
+        Mode 0 - normal mode, output from GMS goes to PBR (255)
+        Mode 1 - reset mode, N2 (nitrogen) goes to PBR (239)
+        Mode 2 - no gas input to PBR (249)
+        Mode 3 - output of PBR goes to input of PBR (246)
+
+        :param mode: chosen mode (0 to 3)
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_flow(self, repeats):
+        """
+        Actual flow being send from GAS to the PBR.
+
+        :param repeats: the number of measurement repeats
+        :return: The current flow in L/min.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_flow_target(self):
+        """
+        Actual desired flow.
+
+        :return: The desired flow in L/min.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def set_flow_target(self, flow):
+        """
+        Set flow we want to achieve.
+
+        :param flow: flow in L/min we want to achieve (max given by get_flow_max)
+        :return: True if was successful, False otherwise.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_flow_max(self):
+        """
+        Maximal allowed flow.
+
+        :return: The maximal flow in L/min
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def get_pressure(self, repeats=5, wait=0):
+        """
+        Current pressure.
+
+        :param repeats: the number of measurement repeats
+        :param wait: waiting time between individual repeats
+        :return: Current pressure in ???
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
+    def measure_all(self):
+        """
+        Measures all basic measurable values.
+        """
+        raise NotImplementedError("The method is not allowed on this device type")
+
