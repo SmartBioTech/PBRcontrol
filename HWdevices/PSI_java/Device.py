73fe3847a9765d7568ac99aa91afbaa91006dce4
Daniel Magoc <xmagoc@fi.muni.cz>
2020-03-06 17:10:26 +0100

    all JVM dependent devices are initiated in the same thread -> should fix issue #24 :(

--- HWdevices/PSI_java/Device.py
+++ HWdevices/PSI_java/Device.py
@@ -19,8 +19,10 @@ class Device:
     def connect(self, device_config):
         jvm_controller = JVMController()
         if not jvm_controller.isJVMStarted():
-            jpype.addClassPath('HWdevices/PSI_java/lib/jar/bioreactor-commander-0.8.7.jar')
             jvm_controller.startJVM()
+
+        if not jpype.isThreadAttachedToJVM():
+            jpype.attachThreadToJVM()
 
         CommanderConnector = jpype.JClass("psi.bioreactor.commander.CommanderConnector")
         device = CommanderConnector(device_config, self.address, 115200)
@@ -33,6 +35,7 @@ class Device:
 
     def disconnect(self):
         self.device.disconnect()
+        jpype.detachThreadFromJVM()
 
     def __str__(self):
         return self.ID + " @ " + str(self.address)
--- HWdevices/PSI_java/jvm.py
+++ HWdevices/PSI_java/jvm.py
@@ -15,11 +15,8 @@ from utils.singleton import singleton
 
 @singleton
 class JVMController:
-    def __init__(self):
-        self.is_started = False
 
     def startJVM(self):
-        self.is_started = True
         jpype.addClassPath('HWdevices/PSI_java/lib/jar/bioreactor-commander-0.8.7.jar')
         jpype.startJVM(jvmpath=jpype.getDefaultJVMPath(), convertStrings=False,
                        classpath="HWdevices/PSI_java/lib/jar/bioreactor-commander-0.8.7.jar")
@@ -28,4 +25,4 @@ class JVMController:
         jpype.shutdownJVM()
 
     def isJVMStarted(self):
-        return jpype.isJVMStarted() and self.is_started
+        return jpype.isJVMStarted()
